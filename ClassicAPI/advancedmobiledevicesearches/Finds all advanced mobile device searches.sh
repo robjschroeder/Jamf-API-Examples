0db49e8db2e8155d72bd3b5153981c08739f0a79
Robert Schroeder <robjschroeder@gmail.com>
2022-10-13 08:45:47 -0700

    Create Finds all advanced mobile device searches.sh

---
+++ ClassicAPI/advancedmobiledevicesearches/Finds all advanced mobile device searches.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Classic API > advancedmobiledevicesearches > Finds all advanced mobile device searches
+#
+# Created 10.13.2022 @robjschroeder
+
+##################################################
+# Variables -- edit as needed
+
+# Jamf Pro API Credentials
+jamfProAPIUsername=""
+jamfProAPIPassword=""
+jamfProURL=""
+
+# Token declarations
+token=""
+tokenExpirationEpoch="0"
+
+#
+##################################################
+
+# Encode credentials
+encodedCredentials=$( printf "${jamfProAPIUsername}:${jamfProAPIPassword}" | /usr/bin/iconv -t ISO-8859-1 | /usr/bin/base64 -i - )
+authToken=$(/usr/bin/curl -s -H "Authorization: Basic ${encodedCredentials}" "${jamfProURL}"/api/v1/auth/token -X POST)
+token=$(/bin/echo "${authToken}" | /usr/bin/plutil -extract token raw -)
+tokenExpiration=$(/bin/echo "${authToken}" | /usr/bin/plutil -extract expires raw - | /usr/bin/awk -F . '{print $1}')
+tokenExpirationEpoch=$(/bin/date -j -f "%Y-%m-%dT%T" "${tokenExpiration}" +"%s")
+/bin/echo "Token will expire: ${tokenExpiration}"
+
+# Finds all advanced mobile device searches
+curl --request GET \
+--url ${jamfProURL}/JSSResource/advancedmobiledevicesearches \
+--header 'accept: application/xml' \
+--header "Authorization: Bearer ${token}"
+
+
+# Invalidate token
+responseCode=$(/usr/bin/curl -w "%{http_code}" -H "Authorization: Bearer ${token}" ${jamfProURL}/api/v1/auth/invalidate-token -X POST -s -o /dev/null)
+if [[ ${responseCode} == 204 ]]; then
+	/bin/echo "" && /bin/echo "Token successfully invalidated"
+	token=""
+	tokenExpirationEpoch="0"
+elif [[ ${responseCode} == 401 ]]; then
+	/bin/echo "" && /bin/echo "Token already invalid"
+else
+	/bin/echo "" && /bin/echo "An unknown error occurred invalidating the token"
+fi
+
+exit 0
