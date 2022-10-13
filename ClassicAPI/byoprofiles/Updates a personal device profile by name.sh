bf312c8bcf44d6b6d51cfb192dbd0f5c6f22f172
Robert Schroeder <robjschroeder@gmail.com>
2022-10-13 11:20:36 -0700

    Create Updates a personal device profile by name.sh

---
+++ ClassicAPI/byoprofiles/Updates a personal device profile by name.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Classic API > byoprofiles > Updates a personal device profile by name
+#
+# Created 10.13.2022 @robjschroeder
+
+##################################################
+# Variables -- edit as needed
+
+# Jamf Pro API Credentials
+jamfProAPIUsername=""
+jamfProAPIPassword=""
+jamfProURL=""
+
+# Name to filter by
+name=""
+
+# Sample Request Body, modify to fit your needs
+requestBody="<byoprofile>
+    <general>
+        <name>Personal Device Profile</name>
+        <site>
+            <id>-1</id>
+            <name>None</name>
+        </site>
+        <enabled>true</enabled>
+        <description>Used for Android or iOS BYO device enrollments</description>
+    </general>
+</byoprofile>"
+
+# Token declarations
+token=""
+tokenExpirationEpoch="0"
+
+#
+##################################################
+
+# Encode credentials
+encodedCredentials=$( printf "${jamfProAPIUsername}:${jamfProAPIPassword}" | /usr/bin/iconv -t ISO-8859-1 | /usr/bin/base64 -i - )
+authToken=$(/usr/bin/curl -s -H "Authorization: Basic ${encodedCredentials}" "${jamfProURL}"/api/v1/auth/token -X POST)
+token=$(/bin/echo "${authToken}" | /usr/bin/plutil -extract token raw -)
+tokenExpiration=$(/bin/echo "${authToken}" | /usr/bin/plutil -extract expires raw - | /usr/bin/awk -F . '{print $1}')
+tokenExpirationEpoch=$(/bin/date -j -f "%Y-%m-%dT%T" "${tokenExpiration}" +"%s")
+/bin/echo "Token will expire: ${tokenExpiration}"
+
+# Update personal device profile with request body
+curl --request PUT \
+--url ${jamfProURL}/JSSResource/byoprofiles/name/${name} \
+--header 'Accept: application/xml' \
+--header 'Content-Type: application/xml' \
+--header "Authorization: Bearer ${token}" \
+--data "${requestBody}"
+
+
+# Invalidate token
+responseCode=$(/usr/bin/curl -w "%{http_code}" -H "Authorization: Bearer ${token}" ${jamfProURL}/api/v1/auth/invalidate-token -X POST -s -o /dev/null)
+if [[ ${responseCode} == 204 ]]; then
+	/bin/echo "" && /bin/echo "Token successfully invalidated"
+	token=""
+	tokenExpirationEpoch="0"
+elif [[ ${responseCode} == 401 ]]; then
+	/bin/echo "" && /bin/echo "Token already invalid"
+else
+	/bin/echo "" && /bin/echo "An unknown error occurred invalidating the token"
+fi
+
+exit 0
